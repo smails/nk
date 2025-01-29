36b6482a25612c7c3eb2201edb7a830fd987de22
smails <maxon2504@gmail.com>
2025-01-29 16:05:54 +0300

    new page

--- src/modules/offer/offer.js
+++ src/modules/offer/offer.js
@@ -23,3 +23,24 @@ $('.offer').each((index, item) => {
     }
   });
 })
+
+$('.offer_sale').each((index, item) => {
+  const swiper = new Swiper($(item).find('.swiper')[0], {
+    slidesPerView: 1.15,
+    spaceBetween: '10px',
+    navigation: {
+      nextEl: $(item).find('.slider-arrow_next')[0],
+      prevEl: $(item).find('.slider-arrow_prev')[0],
+    },
+    modules: [Navigation],
+    breakpoints: {
+      767: {
+        slidesPerView: 2.15,
+      },
+      1500: {
+        spaceBetween: '15px',
+        slidesPerView: 3,
+      },
+    }
+  });
+})
---
+++ src/modules/select-room/script.js
@@ -0,0 +1,25 @@
+import $ from 'jquery'
+import Swiper from 'swiper';
+import { Pagination } from 'swiper/modules';
+
+
+
+$('.sp-prod-card__slider').each((index, item) => {
+  const swiper = new Swiper($(item).find('.swiper')[0], {
+    slidesPerView: 1,
+      pagination: {
+        el: $(item).find('.slider-dots')[0],
+      },
+      modules: [Pagination],
+  });
+})
+
+$('.filter__full-filter').click(() => {
+  $('body').addClass('fixed')
+  $('.filter').addClass('fixed')
+})
+
+$('.filter__close').click(() => {
+  $('.filter').removeClass('fixed')
+  $('body').removeClass('fixed')
+})
