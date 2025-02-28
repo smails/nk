7d32478c453bac6206ba0a1fe13fbaf474c74862
smails <maxon2504@gmail.com>
2025-02-28 07:50:11 +0300

    bug fix

--- src/static/js/core.js
+++ src/static/js/core.js
@@ -1,1 +1,19 @@
 import $ from 'jquery'
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+
+
+
+$('.manufacure-tabs__wrapper').each((index, item) => {
+  const swiper = new Swiper($(item).find('.swiper')[0], {
+    slidesPerView: 1,
+    navigation: {
+      nextEl: $(item).find('.slider-arrow_next')[0],
+      prevEl: $(item).find('.slider-arrow_prev')[0],
+    },
+    pagination: {
+      el: $(item).find('.slider-dots')[0],
+    },
+    modules: [Navigation, Pagination],
+  });
+})
