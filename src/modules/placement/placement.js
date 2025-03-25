3ec6aba2e51077a133f1ac6f56511e9b0e8625ae
smails <maxon2504@gmail.com>
2025-03-25 15:36:56 +0300

    new page

--- src/modules/placement/placement.js
+++ src/modules/placement/placement.js
@@ -5,3 +5,11 @@ $('.placement__tab').click(function () {
   $('.placement__box').hide();
   $('.placement__box').eq($(this).index()).fadeIn();
 })
+
+
+$('.placement-chess__frame_available').click((e) =>{
+  $('.placement-chess__product-info').removeClass('open').css({
+    left: e.clientX,
+    top: e.clientY -($('.placement-chess__product-info').height() / 2),
+  }).toggleClass('open')
+})
